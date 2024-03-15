fe627b15d31a2f9abc6dfbc0b9b75f77ef129926
Daniel <danielcgilibert@gmail.com>
2024-03-15 17:05:09 +0100

    Add new files and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
+import db from "@astrojs/db";
+import vercel from "@astrojs/vercel/serverless";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    optimizeDeps: {
+      exclude: ['oslo']
+    }
+  },
+  integrations: [db(), tailwind()],
+  output: "server",
+  adapter: vercel()
+});
